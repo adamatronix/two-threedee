492d9fe95df1f0c45cff883d203f82405652f915
Your Name <you@example.com>
2021-07-09 16:00:06 +0800

    p5-setup Add basic project with p5 canvas

---
+++ src/TwoThreeDee.ts
@@ -0,0 +1,32 @@
+import P5 from 'p5';
+
+class TwoThreeDee {
+  container: HTMLDivElement;
+
+  // Normal signature with defaults
+  constructor(container: HTMLDivElement) {
+    this.container = container;
+
+    new P5(this.sketch);
+
+  }
+
+  sketch = (p5: P5) => {
+
+    // The sketch setup method 
+    p5.setup = () => {
+      // Creating and positioning the canvas
+      const canvas = p5.createCanvas(200, 200);
+      canvas.parent(this.container);
+      // Configuring the canvas
+      p5.background("white");
+    };
+  
+    // The sketch draw method
+    p5.draw = () => {
+   
+    };
+  };
+}
+
+export { TwoThreeDee };
